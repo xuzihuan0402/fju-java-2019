efd4b78ee751db9997659eed8a161062e236dea6
user <user@es301n13>
2019-06-04 11:57:37 +0800

    0604

--- myproject/src/com/tom/kkk/Main.java
+++ myproject/src/com/tom/kkk/Main.java
@@ -3,16 +3,22 @@ package com.tom.kkk;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		Breakfast breakfast = new Breakfast();
+
+        System.out.println("Hello World");
+        Breakfast breakfast = new Breakfast();
         breakfast.start();
 
         BrushTeeth bt = new BrushTeeth();
         Thread thread = new Thread(bt);
         thread.start();
-		
+        try {
+            thread.join();
+            breakfast.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-	}
+        System.out.println("Main end");
+    }
 
 }
--- myproject/src/com/tom/kkk/MyFrame.java
+++ myproject/src/com/tom/kkk/MyFrame.java
@@ -1,12 +1,20 @@
 package com.tom.kkk;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MyFrame extends JFrame{
 	
 	public MyFrame(){
         setSize(500,300);
         setVisible(true);
+        JButton ok = new JButton("OK");
+        JPanel north = new JPanel();
+        north.add(ok);
+        add(north, BorderLayout.NORTH);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
