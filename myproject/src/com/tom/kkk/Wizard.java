82a6ba9ffcc39cd1d80086ecafa4181c8afdc4c5
xuzihuan0402 <xu13310101131@gmail.com>
2019-06-08 21:44:17 +0800

    0

--- myproject/src/com/tom/kkk/Wizard.java
+++ myproject/src/com/tom/kkk/Wizard.java
@@ -2,29 +2,31 @@ package com.tom.kkk;
 
 public class Wizard extends Thread{
 	
+	@Override
 	public void run() {
 		thunder();
 		}
-
-	public synchronized void thunder() {
-		
-			System.out.println("Thunder Start!");
-			try {
-				sleep(2000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			System.out.println("Thunder End!");
-			}
-
-
-
+	 public synchronized void thunder(){
+		 System.out.println("THUNDER!!");
+		 try {
+			 sleep(2000);
+			 } catch (InterruptedException e) {
+				 e.printStackTrace();
+				 }
+		 System.out.println("END");
+		 }
+	
+	
+	
+	
+			
 	public static void main(String[] args) {
 		Wizard w1 = new Wizard();
 		Thread t1 = new Thread(w1);
 		t1.start();
 		Wizard w2 = new Wizard();
 		Thread t2 = new Thread(w2);
-		t2.start();	}
+		t2.start();
+		}
 
 }
